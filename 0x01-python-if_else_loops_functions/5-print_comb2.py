4495564559637ccf49b5ada9b6a941607077ea06
tebogoditse <tebogoditse2@gmail.com>
2024-03-27 15:45:23 +0200

    print comb2

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,6 +2,6 @@
 
 for number in range(00, 100):
     if number == 99:
-        print("{}".format(number))
+        print("{d}".format(number))
     else:
-        print("{:02}".format(number), end = ", ")
+        print("{:02d}".format(number), end = ", ")
