53cbbb3d60855345e8289c848e773e1e7661b513
tebogoditse <tebogoditse2@gmail.com>
2024-04-01 13:51:38 +0200

    print comb ascending

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,6 @@
 print("{:02d}".format(0), end="")
 for number in range(1, 90):
     if str("{:02d}".format(number))[0] < str("{:02d}".format(number))[1]:
-        print(f", {number}", end="")
+        print(f", {number:02d}", end="")
 
 print(end='\n')
