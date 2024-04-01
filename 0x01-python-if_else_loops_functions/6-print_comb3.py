b7eb47fb32a9e00cf88ec6f63fd607665198c47c
tebogoditse <tebogoditse2@gmail.com>
2024-04-01 13:48:52 +0200

    print comb ascending

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 
-first_digit = 0
-print("{:02d}".format(first_digit), end="")
+print("{:02d}".format(0), end="")
 for number in range(1, 90):
-    number = "{:02d}".format(number)
-    if str(number)[0] < str(number)[1]:
+    if str("{:02d}".format(number))[0] < str("{:02d}".format(number))[1]:
         print(f", {number}", end="")
 
 print(end='\n')
