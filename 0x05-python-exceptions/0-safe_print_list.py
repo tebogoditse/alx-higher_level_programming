924a9ebc62b2dda68969c1a1a5ad95a630666f92
tebogoditse <tebogoditse2@gmail.com>
2024-04-03 01:51:35 +0200

    print list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,8 +6,7 @@ def safe_print_list(my_list=[], x=0):
     for i in range(0, x):
         try:
             print("{}".format(my_list[i]), end="")
-            i += 1
-        except NameError:
+        except IndexError:
             continue
         else:
             count += 1
