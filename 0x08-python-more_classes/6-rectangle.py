bf9f643c4b706d708a39c6256cd6edbc2b5e1b62
tebogoditse <tebogoditse2@gmail.com>
2024-04-09 01:54:17 +0200

    Write a class Rectangle that defines a rectangle by: (based on 5-rectangle.py)

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -10,7 +10,7 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
-        self.number_of_instances += 1
+        type(self).number_of_instances += 1
 
     @property
     def width(self):
@@ -61,4 +61,4 @@ class Rectangle:
 
     def __del__(self):
         print("Bye rectangle...")
-        self.number_of_instances -= 1
+        type(self).number_of_instances -= 1
