31bf51a0c88a1918d7a459872e2970091928bfcd
tebogoditse <tebogoditse2@gmail.com>
2024-04-15 22:37:56 +0200

    checks type of obj

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""function that validated whether obj is instance of specific class"""
+
+def is_same_class(obj, a_class):
+    """checking"""
+
+    if type(obj) == a_class:
+        return True
+    else:
+        return False
