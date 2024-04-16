5414fc8367e58d01f73dbe3667139871cf6dc203
tebogoditse <tebogoditse2@gmail.com>
2024-04-16 17:18:24 +0200

    first-commit

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+"""function that reads and prints a text file"""
+
 
 def read_file(filename=""):
+    """Reads and prints"""
+
     with open(filename, "r", encoding="UTF-8") as file:
         content = file.read()
         print(content, end="")
