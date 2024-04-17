c61b379fcfa8cfb7351fe06777eb24bd77f0f36d
tebogoditse <tebogoditse2@gmail.com>
2024-04-17 03:22:10 +0200

    append after

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Search and update"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """
+    function that inserts a line of text to a file,
+    after each line containing a specific string
+    """
+
+    text = ""
+    with open(filename) as file:
+        for line in file:
+            text += line
+            if search_string in line:
+                text += new_string
+
+    with open(filename, "a") as wrt:
+        wrt.write(text)
