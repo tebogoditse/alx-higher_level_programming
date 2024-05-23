7a92d88ead0cff66c95b312ba43c2243d1c63952
tebogoditse <tebogoditse2@gmail.com>
2024-05-23 03:31:07 +0200

    all

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,9 @@
+-- Creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa)
+
+-- DATABASE
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- TABLE
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+    id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    name VARCHAR(256) NOT NULL
+);
---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,11 @@
+-- Creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa) 
+
+-- DATABASE
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- TABLE
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+	id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	state_id INT NOT NULL,
+	FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id),
+	name VARCHAR(256) NOT NULL
+);
