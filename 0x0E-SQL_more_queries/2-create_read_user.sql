1e43944d3801dd769b96d9a2df07751c634cdc18
tebogoditse <tebogoditse2@gmail.com>
2024-05-23 02:28:51 +0200

    2-create_read_user.sql

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,9 @@
+-- Creates the database hbtn_0d_2 and the user user_0d_2
+-- Database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+-- User
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' 
+IDENTIFIED BY 'user_0d_2_pwd';
+-- Select priviledges
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES
