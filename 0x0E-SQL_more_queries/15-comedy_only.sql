bc7c5c084c88608fa8e496bd54ca807f630e1bee
tebogoditse <tebogoditse2@gmail.com>
2024-05-24 04:01:54 +0200

    15-comedy_only.sql

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,3 +1,4 @@
+-- Lists all Comedy shows in the database hbtn_0d_tvshows
 SELECT title FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id=tv_show_genres.show_id
 JOIN tv_genres ON tv_genres.id=tv_show_genres.genre_id
