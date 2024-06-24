f37d653307479deb1e293c8f8c0b15b5206edee2
tebogoditse <tebogoditse2@gmail.com>
2024-06-24 23:29:34 +0200

    6-multi_languages_loop.js

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const multiLanguage = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let i = 0;
+
+for (; i < multiLanguage.length; i++) {
+  console.log(multiLanguage[i]);
+}
