b98ab6c85a7169fb2192fe44bf5eaf2ab20f1419
tebogoditse <tebogoditse2@gmail.com>
2024-06-26 23:00:27 +0200

    11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,10 +2,10 @@
 
 const process = require('process');
 
-if (process.argv.length >= 4) {
+if (process.argv.length < 4) {
+  console.log(0);
+} else if (process.argv.length >= 4) {
   const list = process.argv.sort();
   list.reverse();
   console.log(list[1]);
-} else {
-  console.log(0);
 }
