a953de13192d768a27ea98c585f4f5b48127ac34
tebogoditse <tebogoditse2@gmail.com>
2024-07-05 17:08:04 +0200

    0-hbtn_status.py

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+from urllib.request import Request, urlopen
+
+if __name__ == "__main__":
+    req = Request("https://alx-intranet.hbtn.io/status")
+    with urlopen(req) as res:
+        body = res.read()
+        print("Body response:")
+        print(f"\t- type: {type(body)}")
+        print(f"\t- content: {body}")
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
