3b7064304b2525097620ee8e86cc5a27d59e1ec4
tebogoditse <tebogoditse2@gmail.com>
2024-07-06 00:54:06 +0200

    2-post_email.py

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL and an email,
+sends a POST request to the passed URL with the email as a parameter,
+and displays the body of the response (decoded in utf-8)
+"""
+
+import sys
+from urllib.request import Request, urlopen
+from urllib.parse import urlencode
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+    data = urlencode(value).encode("ascii")
+    req = Request(url, data)
+
+    with urlopen(req) as res:
+        print(res.read().decode("uft-8"))
