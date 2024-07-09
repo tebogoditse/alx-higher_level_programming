f493d1fe1e500897292a7458efab9dd37c2cb355
tebogoditse <tebogoditse2@gmail.com>
2024-07-10 01:35:30 +0200

    all

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Function that finds a peak in a list of unsorted integers."""
+
+def find_peak(list_of_integers):
+    max_i = None
+    for i in list_of_integers:
+        if max_i is None or max_i < i:
+            max_i = i
+    return max_i
