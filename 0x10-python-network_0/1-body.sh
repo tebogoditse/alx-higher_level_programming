a0458d346c82e7187a63cecc740b8a0d5f856d11
tebogoditse <tebogoditse2@gmail.com>
2024-07-09 03:18:17 +0200

    1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-curl -sL "$1"
+if [ $(curl -L -s -X HEAD -w "%{http_code}" "$1") == '200' ]; then curl -Ls "$1"; fi
